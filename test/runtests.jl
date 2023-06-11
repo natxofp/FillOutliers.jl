3d406165bc4e7dfb9c5cd986fa753b7c1470e8c8
natxofp <ignasi.fernandez@chalmers.se>
2023-06-11 12:38:21 +0200

    fixed_tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,13 @@ using Test
 
     # Test mean method
     expected_mean = [1.0, 2.0, 100000.0, 4.0, 5.0, 7.0, 9.0, 15.0, 11.5, 8.0, 9.0]
-    @test fill_outliers(data, "mean") ≈ expected_mean
+    @test filloutliers(data, "mean") ≈ expected_mean
 
     # Test quartiles method
     expected_quartiles = [1.0, 2.0, 3.0, 4.0, 5.0, 7.0, 9.0, 15.0, 11.5, 8.0, 9.0]
-    @test fill_outliers(data, "quartiles") ≈ expected_quartiles
+    @test filloutliers(data, "quartiles") ≈ expected_quartiles
 
     # Test moving mean method
     expected_moving_mean = [1.0, 2.0, 100000.0, 4.0, 5.0, 7.0, 9.0, 15.0, 11.5, 8.0, 9.0]
-    @test fill_outliers(data, "moving_mean") ≈ expected_moving_mean
+    @test filloutliers(data, "moving_mean") ≈ expected_moving_mean
 end
